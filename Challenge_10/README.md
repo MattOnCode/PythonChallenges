12070ce1afc3a630384e99b0bb64e43e93cbed38
Matt <mattjrx@gmail.com>
2017-11-17 09:31:46 +0000

    Started work on Challenge 10

---
+++ Challenge_10/README.md
@@ -0,0 +1,5 @@
+# Challenge 10
+
+## Description:
+
+Create a solution that asks the user for numbers until a ’q’ or a ’Q’ is typed. The numbers should be appended to a list and the program should then calculate and print the average of the numbers.
---
+++ Challenge_10/input_average.py
@@ -0,0 +1,35 @@
+import re
+import unittest
+
+numbers = []
+
+
+def validate_input(user_input):
+    """
+    Validates whether the input is a number, or whether it is Q or q
+    which will finish the application.
+    :param user_input: The users inputted value.
+    :return: True/False dependent on whether it is Q/q or not.
+    """
+    return False if re.search("[Qq]", user_input) else True
+
+
+def calculate_average(number_list):
+    """
+    Calculates the average of all the numbers within a list.
+    :param number_list: The list of numbers.
+    :return: The average of the numbers.
+    """
+    numbers_sum = 0
+    for number in number_list:
+        numbers_sum += number
+    return numbers_sum / len(numbers)
+
+
+while True:
+    input_number = input("Enter a number: ")
+    if validate_input(input_number):
+        numbers.append(int(input_number))
+    else:
+        print("Average: {}".format(calculate_average(numbers)))
+        break
